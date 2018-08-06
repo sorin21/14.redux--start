616428223d90980821cc4bbddd64514a194723dd
Sorinel Dragomir <sorinel.dragomir@vauban.ro>
2018-08-06 15:38:14 +0300

    first commit

--- src/redux-basics/redux-basics.js
+++ src/redux-basics/redux-basics.js
@@ -28,9 +28,13 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+// Subscription
+store.subscribe(() => {
+  console.log('Subscription', store.getState());
+});
+
 // Action
 store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', payload: 10});
 console.log(store.getState());
 
-// Subscription
--- src/redux-basics/redux-basics2.js
+++ src/redux-basics/redux-basics2.js
@@ -30,3 +30,5 @@ console.log(store.getState());
 store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', payload: 10});
 console.log(store.getState());
+
+//
